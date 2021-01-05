85b96d98860b8b4387a7392cd12ca823863c1b67
freefireman <beluh.c.s@gmail.com>
2021-01-05 20:35:09 +0200

    little style fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,23 @@
 import React, {Component} from 'react';
-import { Link, Switch, withRouter,Route} from 'react-router-dom';
+import {Link, Switch, withRouter, Route} from 'react-router-dom';
 import logo from '../logo.svg';
 import Calculator from './Calculator';
 
 class Header extends Component {
 
-	render() {
-		return (
-			<div>
-			<header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-				<Link to={'/calculator'}>calc</Link>
-      </header>
-				<Switch>
-					<Route path={'/calculator'} component={Calculator}/>
-				</Switch>
-			</div>
-		 );
-	}
+    render() {
+        return (
+            <div>
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <Link to={'/calculator'}>calc</Link>
+                </header>
+                <Switch>
+                    <Route path={'/calculator'} component={Calculator}/>
+                </Switch>
+            </div>
+        );
+    }
 }
 
 export default withRouter(Header);
