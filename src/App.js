a7125fdb1dc1803d79537fc3f5736512cdb17073
freefireman <beluh.c.s@gmail.com>
2021-01-05 18:48:29 +0200

    dsd

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-
 import './App.css';
 
-import {
-  withRouter,
-  Switch,
-  Route,
-  Link
-} from 'react-router-dom';
+import {withRouter} from 'react-router-dom';
 import Header from './components/Header';
 
 
 function App() {
-  return (
-    <div className="App">
-      <Header/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+        </div>
+    );
 }
 
 export default withRouter(App);
